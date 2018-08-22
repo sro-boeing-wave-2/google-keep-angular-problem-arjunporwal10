9d15bd13760c2afc0db924b958e1538d0ae83594
arjun <arjunporwal10@gmail.com>
2018-08-22 12:38:53 +0530

    crud working

--- googlekeep/src/app/notes/notes.component.ts
+++ googlekeep/src/app/notes/notes.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import{Note} from '../notes';
 import{NotesArray} from '../mock-notes';
 import{NoteService} from '../note.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,Router } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormGroup, FormControl, FormBuilder, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
@@ -13,10 +14,39 @@ export class NotesComponent implements OnInit {
 
   notesArray :Note[];
 
+  NotePostForm = this.fb.group({
+    NotesId: [''],
+    Title: [''],
+    Text: [''],
+    IsPinned: [''],
+    CheckLists: this.fb.array([]),
+    Labels: this.fb.array([])
+  });
+
+  get Labels() {
+    return this.NotePostForm.get('Labels') as FormArray;
+  }
+  addLabel() {
+    this.Labels.push(this.fb.group({
+      labelName: ['']
+    }));
+  }
+  get CheckLists() {
+    return this.NotePostForm.get('CheckLists') as FormArray;
+  }
+  addCheckList() {
+    this.CheckLists.push(this.fb.group({
+      ChecklistData: [''],
+      IsChecked: ['']
+    }));
+  }
+
   constructor(
     private route: ActivatedRoute,
     private noteService: NoteService,
-    private location: Location
+    private location: Location,
+    private fb: FormBuilder,
+    private router:Router
   ) {}
 
   ngOnInit() {
@@ -27,19 +57,11 @@ export class NotesComponent implements OnInit {
      this.noteService.getNotes()
      .subscribe(notes => this.notesArray = notes);
   }
-  add(title: string): void {
-    title = title.trim();
-    if (!title) { return; }
-    this.noteService.addNote({
-
-           notesId:5,
-           Text: 'BlackCurrent',
-           Title:title,
-           IsPinned:false,
-           Labels:[{LabelName:'label1',id:1},{LabelName:'label2',id:2}],
-           CheckLists:[{CheckListData:'checklist1',id:1,IsChecked:true},{CheckListData:'checklist2',id:2,IsChecked:true}] }  as Note)
+  add(NoteToPost:Note): void {
+    this.noteService.addNote(NoteToPost)
       .subscribe(note => {
         this.notesArray.push(note);
+        this.router.navigate(['']);
       });
   }
   delete(id: string): void {
@@ -48,5 +70,8 @@ export class NotesComponent implements OnInit {
     this.noteService.deleteNote(id).subscribe();
   }
 
+  post():void{
+    this.add(this.NotePostForm.value);
+  }
 
 }
